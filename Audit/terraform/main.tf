c0ff017db3020a8cb404a84cd86edc1e08a10b4d
Carlos Enriquez <cae@amazon.com>
2024-12-27 15:47:18 -0500

    cambios charly 24

--- Audit/terraform/main.tf
+++ Audit/terraform/main.tf
@@ -1,88 +1,90 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-module "s3-access-logs-bucket" {
-    source = "./aft-ca-bac/modules/s3-access-logs-bucket"
+# # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# # SPDX-License-Identifier: Apache-2.0
+# #
+# module "s3-access-logs-bucket" {
+#     source = "./aft-ca-bac/modules/s3-access-logs-bucket"
 
-        aws_region = "us-east-1"
-        account_id = data.aws_caller_identity.current.account_id
-}
+#         aws_region = "us-east-1"
+#         account_id = data.aws_caller_identity.current.account_id
+# }
 
-module "s3-access-logs-bucket-dr" {
-    source = "./aft-ca-bac/modules/s3-access-logs-bucket-dr"
+# module "s3-access-logs-bucket-dr" {
+#     source = "./aft-ca-bac/modules/s3-access-logs-bucket-dr"
 
-        aws_region = "us-east-2"
-        account_id = data.aws_caller_identity.current.account_id
-    providers = {
-        aws = aws.DR
-        }
-}
+#         aws_region = "us-east-2"
+#         account_id = data.aws_caller_identity.current.account_id
+#     #providers aws.DR file aft-providers-dr.jinja
+#     providers = {
+#         aws = aws.DR
+#         }
+# }
 
-# module "aft-ca-development" {
+# # module "aft-ca-development" {
 
-#     source = "./aft-ca-bac/ca-development"
+# #     source = "./aft-ca-bac/ca-development"
+
+# #     aws_region  = "us-east-1"
+# #     service     = "aws-private-ca-dev"
+# #     direccion   = "D.R. Transformacion Digital"
+# #     gerencia    = "Transformacion Digital"
+# #     proyecto    = "MSN-5843"
+# #     centroCostos = "5053SSC"
+# #     responsable = "Jose Manuel Paez"
+# #     ambiente    = "Desarrollo"
+# #     servicio    = "Banca Digital"
+# #     idCargoSAP  = "REG.OP.24.101.03"
+# #     s3_logging_bucket_id = module.s3-access-logs-bucket.s3_logging_bucket_id
+# # }
+
+# module "aft-ca-production" {
+
+#     source = "./aft-ca-bac/ca-production"
 
 #     aws_region  = "us-east-1"
-#     service     = "aws-private-ca-dev"
+#     service     = "aws-private-ca-prod"
 #     direccion   = "D.R. Transformacion Digital"
 #     gerencia    = "Transformacion Digital"
 #     proyecto    = "MSN-5843"
 #     centroCostos = "5053SSC"
 #     responsable = "Jose Manuel Paez"
-#     ambiente    = "Desarrollo"
+#     ambiente    = "Produccion"
 #     servicio    = "Banca Digital"
 #     idCargoSAP  = "REG.OP.24.101.03"
 #     s3_logging_bucket_id = module.s3-access-logs-bucket.s3_logging_bucket_id
 # }
 
-module "aft-ca-production" {
+# # module "aft-ca-production-dr" {
 
-    source = "./aft-ca-bac/ca-production"
+# #     source = "./aft-ca-bac/ca-production-dr"
 
-    aws_region  = "us-east-1"
-    service     = "aws-private-ca-prod"
-    direccion   = "D.R. Transformacion Digital"
-    gerencia    = "Transformacion Digital"
-    proyecto    = "MSN-5843"
-    centroCostos = "5053SSC"
-    responsable = "Jose Manuel Paez"
-    ambiente    = "Produccion"
-    servicio    = "Banca Digital"
-    idCargoSAP  = "REG.OP.24.101.03"
-    s3_logging_bucket_id = module.s3-access-logs-bucket.s3_logging_bucket_id
-}
-
-module "aft-ca-production-dr" {
-
-    source = "./aft-ca-bac/ca-production-dr"
-
-    aws_region  = "us-east-2"
-    service     = "aws-private-ca-prod-dr"
-    direccion   = "D.R. Transformacion Digital"
-    gerencia    = "Transformacion Digital"
-    proyecto    = "MSN-5843"
-    centroCostos = "5053SSC"
-    responsable = "Jose Manuel Paez"
-    ambiente    = "Produccion"
-    servicio    = "Banca Digital"
-    idCargoSAP  = "REG.OP.24.101.03"
-    s3_logging_bucket_id = module.s3-access-logs-bucket-dr.s3_logging_bucket_id
-    arn         = module.aft-ca-production.root_ca_arn_pr
-
-    providers = {
-        aws = aws.DR
-        aws.oregon = aws
-    }
+# #     aws_region  = "us-east-2"
+# #     service     = "aws-private-ca-prod-dr"
+# #     direccion   = "D.R. Transformacion Digital"
+# #     gerencia    = "Transformacion Digital"
+# #     proyecto    = "MSN-5843"
+# #     centroCostos = "5053SSC"
+# #     responsable = "Jose Manuel Paez"
+# #     ambiente    = "Produccion"
+# #     servicio    = "Banca Digital"
+# #     idCargoSAP  = "REG.OP.24.101.03"
+# #     s3_logging_bucket_id = module.s3-access-logs-bucket-dr.s3_logging_bucket_id
+# #     #arn root ca
+# #     arn         = module.aft-ca-production.root_ca_arn_pr
+# #     #providers file aft-providers-dr.jinja
+# #     providers = {
+# #         aws = aws.DR
+# #         aws.oregon = aws
+# #     }
     
-}
+# # }
 
-# module "aft-auditmanager-tgt"{
-#     source ="./aft-auditmanager-tgt"
+# # module "aft-auditmanager-tgt"{
+# #     source ="./aft-auditmanager-tgt"
 
-#     audit_manager_owner_role = [ 
-#         "arn:aws:iam::374165089831:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AWSIAMAdmin_e542ee68e047edab",
-#         "arn:aws:iam::374165089831:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AWSAdministratorAccess_4246ad7f15449d67"
-#         ]
-#     s3_logging_bucket_id =  module.s3-access-logs-bucket.s3_logging_bucket_id
-#     assessment_account_ids = ["992382711611", "339713170197"]
-# }
+# #     audit_manager_owner_role = [ 
+# #         "arn:aws:iam::374165089831:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AWSIAMAdmin_e542ee68e047edab",
+# #         "arn:aws:iam::374165089831:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AWSAdministratorAccess_4246ad7f15449d67"
+# #         ]
+# #     s3_logging_bucket_id =  module.s3-access-logs-bucket.s3_logging_bucket_id
+# #     assessment_account_ids = ["992382711611", "339713170197"]
+# # }
