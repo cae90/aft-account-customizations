60d31126374954fc3bf63722a657d6a46d76e0ba
Carlos Enriquez <cae@amazon.com>
2025-01-20 11:36:26 -0500

    tags

--- Audit/terraform/aft-auditmanager-tgt/variables.tf
+++ Audit/terraform/aft-auditmanager-tgt/variables.tf
@@ -15,5 +15,43 @@ variable "assessment_account_ids" {
     type    = list(string)
     description = "accounts included in the assessment"
 }
+variable "direccion" {
+    type    = string
+    description = "Tag de la direccion"
+}
 
+variable "gerencia" {
+    type    = string
+    description = "Tag de la gerencia"
+}
+
+variable "proyecto" {
+    type    = string
+    description = "Tag del proyecto"
+}
+
+variable "centroCostos" {
+    type    = string
+    description = "Tag del centro de costos del proyecto"
+}
+
+variable "responsable" {
+    type    = string
+    description = "Tag de responsable del proyecto"
+}
+
+variable "ambiente" {
+    type    = string
+    description = "Tag de ambiente de despliegue"
+}
+
+variable "servicio" {
+    type    = string
+    description = "Tag del area de servicio"
+}
+
+variable "idCargoSAP" {
+    type    = string
+    description = "Tag del ID de Cargo SAP"
+}
 
